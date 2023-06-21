e0060cd8bdb9a6f65db182e5c6eebca502aa082c
bumlev <bumwelevy@yahoo.in>
2023-06-21 03:05:57 +0300

    Single Responsibility

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -2,37 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
+use App\Repositories\FilesCtrlRepos\GetSoredImage\Classes\GetSoredImage;
+
 class FilesController extends Controller
 {
-   
+   // Get a stored Image
     public function getStoredImage($imageName)
     {
-        $imagePath = 'public/images/'.$imageName;
-        $extensions = ["jpeg" , "jpg" , "png" , "gif"];
-        $extension = pathinfo($imageName , PATHINFO_EXTENSION);
-
-        // Check if the image exists
-        if (Storage::exists($imagePath)) {
-
-            // Retrieve the image contents
-            $imageContents = Storage::get($imagePath);
-
-            if(in_array($extension , $extensions))
-            {
-                $resizedImage= Image::make($imageContents)->resize(170 , 170);
-                $imageContents = $resizedImage->encode();
-            }
-
-            // Set the response headers
-            $headers = [
-                'Content-Type' => Storage::mimeType($imagePath),
-            ]; 
-            
-            // You can now use the $imageContents variable as needed (e.g., return it as a response or process it further)
-            return Response($imageContents, 200, $headers);
-        }
-        return null;
+        $response = GetSoredImage::execute($imageName);
+        return $response;
     }
 }
---
+++ app/Repositories/FilesCtrlRepos/GetStoredImage/classes/GetStoredImage.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Repositories\FilesCtrlRepos\GetSoredImage\Classes;
+
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+
+class GetSoredImage
+{
+
+    static public function execute($imageName)
+    {
+        $imagePath = 'public/images/'.$imageName;
+        $extensions = ["jpeg" , "jpg" , "png" , "gif"];
+        $extension = pathinfo($imageName , PATHINFO_EXTENSION);
+
+        // Check if the image exists
+        if (Storage::exists($imagePath)) {
+
+            // Retrieve the image contents
+            $imageContents = Storage::get($imagePath);
+
+            if(in_array($extension , $extensions))
+            {
+                $resizedImage= Image::make($imageContents)->resize(170 , 170);
+                $imageContents = $resizedImage->encode();
+            }
+
+            // Set the response headers
+            $headers = [
+                'Content-Type' => Storage::mimeType($imagePath),
+            ]; 
+            
+            // You can now use the $imageContents variable as needed (e.g., return it as a response or process it further)
+            return Response($imageContents, 200, $headers);
+        }
+        return null;
+    }
+}
