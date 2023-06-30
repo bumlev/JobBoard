633737dbcb7d8004d3545cd3624e489801fb0900
Jean Claude GOMBANIRO <gombaniro@hotmail.com>
2023-06-30 10:27:11 +0200

    fix migration, use bigIncrement instead, remove doc dependencies

--- database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
+++ database/migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
@@ -109,7 +109,7 @@ class MigrationCartalystSentinel extends Migration
         });
 
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('email');
             $table->string('password');
             $table->text('permissions')->nullable();
